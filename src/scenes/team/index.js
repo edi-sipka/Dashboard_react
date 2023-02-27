713d353f68554154015d7f23e8410371209cef31
edi-sipka@gmail.com <edisipka@gmail.com>
2023-02-27 16:16:17 +0100

    Created template for header title and subtitle

--- src/scenes/team/index.js
+++ src/scenes/team/index.js
@@ -1,14 +1,14 @@
 import { Box } from "@mui/material"
 import Header from "../../components/Header";
 
-const Dashboard = () => {
+const Team = () => {
     return (
         <Box m="20px">
             <Box display="flex" justifyContent="space-between" alignItems="center">
-                <Header title="DASHBOARD" subtitle="Welcome to the dashboard" />
+                <Header title="TEAM" subtitle="Welcome to the Team page" />
             </Box>
         </Box>
     )
 }
 
-export default Dashboard;
+export default Team;
